8393682db5f2bd9fdbdc76ace5de4f98fd4e3098
srnux <srnux@hotmail.com>
2017-10-14 22:04:15 +0200

    Adding angularcli structure and some test components

---
+++ ngSmartHome/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FetchDataComponent } from './fetchdata/fetchdata.component';
+import { CounterComponent } from './counter/counter.component';
+import { SamuraiComponent } from './samurai/samurai.component'
+import { ProductListComponent } from './products/product-list.component'
+import {TableDemo} from './table/table-demo'
+
+const routes: Routes = [
+    { path: '', redirectTo: 'samurai', pathMatch: 'full' },
+    { path: 'counter', component: CounterComponent },
+    { path: 'fetch-data', component: FetchDataComponent },
+    { path: 'samurai', component: SamuraiComponent },
+    { path: 'home', component: SamuraiComponent },
+    { path: 'products', component: ProductListComponent },
+    {path:'table',component:TableDemo}
+   // { path: '**', redirectTo: 'samurai' }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
